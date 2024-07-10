d837d0dc37046df7046e39d1e91767052672df6d
logeshbaskarlb <logeshbaskar007@gmail.com>
2024-07-10 19:45:01 +0530

    apiUrl

--- src/hooks/UseSignUp.jsx
+++ src/hooks/UseSignUp.jsx
@@ -5,7 +5,7 @@ import { useAuthContext } from "../context/AuthContext";
 const UseHooks = () => {
   const [loading, setLoading] = useState(false);
   const { setAuthUser } = useAuthContext();
-  // const apiUrl = import.meta.env.VITE_API_URL || ""
+  const apiUrl = import.meta.env.VITE_API_URL || ""
 
   const signup = async ({
     fullname,
@@ -24,7 +24,7 @@ const UseHooks = () => {
     if (!success) return;
 
     try {
-      const res = await fetch(`/api/auth/signup`, {
+      const res = await fetch(`${apiUrl}/api/auth/signup`, {
         method: "POST",
         headers: { "Content-type": "application/json" },
         body: JSON.stringify({
